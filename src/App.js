cbaef8c04ebb68ab628d770be3bdef17c0548147
Maciej <31264447+Kratak@users.noreply.github.com>
2018-10-03 15:03:07 +0100

    basic styles
    	fotello
    	background

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        Test
-      </React.Fragment>
+      <>
+      <nav>
+        <i className="icon-home"><span>Home</span></i>
+      </nav>
+      <span>
+        <p><i className="icon-github-squared"></i><a className="aGithub" href="https://github.com/Kratak" target="_blank"> github </a></p>
+				<p><i className="icon-linkedin-squared"></i><a className="alinkedin" href="https://www.linkedin.com/in/maciej-kaczanowski-189404166/" target="_blank"> linkedin </a></p>
+				<p><i className="icon-mail-alt"></i><a className="amail" href="mailto:contact.kaczanowski@gmail.com"> mail </a></p>
+				<p><i className="icon-codeopen"></i><a className="aCodepen" href="https://codepen.io/kratak/#" target="_blank"> codepen </a></p>
+      </span>
+      </>
     );
   }
 }
